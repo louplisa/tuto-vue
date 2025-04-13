13e06683a73da26725b8ac09749580b8a38265bf
aurelie <ferre.aurelie@wanadoo.fr>
2025-04-13 15:57:18 +0200

    The observers and composables

--- src/App.vue
+++ src/App.vue
@@ -1,82 +1,25 @@
 <template>
-  <button @click="showTimer = !showTimer">Afficher / masquer</button>
-  <Timer v-if="showTimer"></Timer>
-  <form action="" @submit.prevent="addTodo">
-    <fieldset role="group">
-      <input
-          v-model="newTodo"
-          placeholder="Tâche à effectuer"
-          type="text"
-      >
-      <button :disabled="newTodo.length === 0">Ajouter</button>
-    </fieldset>
-  </form>
-  <div v-if="todos.length === 0">Vous n'avez pas de tâches à faire :(</div>
-  <div v-else>
-    <ul>
-      <li
-          v-for="todo in sortedTodos"
-          :key="todo.date"
-          :class="{completed: todo.completed}"
-      >
-        <label>
-          <Checkbox :label="todo.title"></Checkbox>
-        </label>
-      </li>
-    </ul>
-    <label>
-      <input v-model="hideCompleted" type="checkbox">
-      Masquer les tâches complétées
-    </label>
-
-    <p v-if="remainingTodos > 0">
-      {{ remainingTodos }} tâche{{ remainingTodos > 1 ? 's' : '' }} à faire
-    </p>
-    <Checkbox :label="`Bonjour`"></Checkbox>
-  </div>
+<input type="text" v-model="page.title">
+  Temps écoulé : {{ time }}
+  <button @click="reset">reset</button>
 </template>
 
 <script setup>
-import {computed, onMounted, ref} from "vue";
-import Checkbox from "@/Checkbox.vue";
-import Button from "@/Button.vue";
-import Layout from "@/Layout.vue";
-import Timer from "@/Timer.vue";
+import {ref, watch, watchEffect} from "vue";
+import {useTimer} from "@/composable/useTimer.js";
 
-const newTodo = ref('')
-const hideCompleted = ref(false)
-const todos = ref([])
-const showTimer = ref(true)
+const {time, reset} = useTimer()
+const name = ref('')
+const page = ref({
+  title: ''
+})
 
-onMounted(() => {
-  fetch('https://jsonplaceholder.typicode.com/todos')
-      .then(r => r.json())
-      .then(v => todos.value = v.map(todo => ({...todo, date: todo.id})))
-})
-const addTodo = () => {
-  todos.value.push({
-    title: newTodo.value,
-    completed: false,
-    date: Date.now()
-  })
-  newTodo.value = ''
-}
-const sortedTodos = computed(() => {
-  const sortedTodos = todos.value.toSorted((a, b) => a.completed > b.completed ? 1 : -1)
-  if (hideCompleted.value === true) {
-    return sortedTodos.filter(t => t.completed === false)
-  }
-  return sortedTodos
-})
-const remainingTodos = computed(() => {
-  return todos.value.filter(t => t.completed === false).length
+// watch(() => page.value.title, (newValue, oldValue) => {
+//   document.title = newValue
+// }, { immediate: true })
+
+watchEffect(() => {
+  document.title = page.value.title
 })
 
 </script>
-
-<style>
-.completed {
-  opacity: .5;
-  text-decoration: line-through;
-}
-</style>
